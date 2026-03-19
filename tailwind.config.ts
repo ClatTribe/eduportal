82b469e23a71c87196f1eeca5b3b02528bc8dacf
ClatTribe <ctribeweb@gmail.com>
2026-03-19 15:48:16 +0530

    feat: Enable Tailwind dark mode class strategy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,14 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     // This is the standard configuration for Next.js App Router:
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './contexts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
