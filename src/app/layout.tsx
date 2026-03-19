6d9685805bc10b5bb9617e7ed3906759470db146
ClatTribe <ctribeweb@gmail.com>
2026-03-19 16:09:09 +0530

    revert: Remove ThemeProvider from layout
    Removed ThemeProvider from layout and adjusted body class.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../../contexts/AuthContext";
-import { ThemeProvider } from "../../contexts/ThemeContext";
 import { Analytics } from '@vercel/analytics/react';
 // import { SpeedInsights } from '@vercel/speed-insights/next';
 
@@ -30,14 +29,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300`}>
-        <ThemeProvider>
-          <AuthProvider>
-            {children}
-            <Analytics />
-            {/* <SpeedInsights /> */}
-          </AuthProvider>
-        </ThemeProvider>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthProvider>
+          {children}
+          <Analytics />
+          {/* <SpeedInsights /> */}
+        </AuthProvider>
       </body>
     </html>
   );
