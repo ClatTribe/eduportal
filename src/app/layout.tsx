1d9eea5ebd7d30a30f64ce2f2bfe2c9ecb7e4656
Saurabh2619 <ctribeweb@gmail.com>
2026-09-06 02:59:11 +0530

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NS86VPDJ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NS86VPDJ" 
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
